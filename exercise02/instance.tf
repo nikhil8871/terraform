0cfd8f8550c8cd122a5e513fa3a6a390b0ea6f29
nikhilsoni <nikhilsoni8815@gmail.com>
2026-09-05 19:08:44 +0530

    Revert "ec2-instance creation"
    This reverts commit 06934b93255f0df6466a1c6fc6259408f9ab6a99.

--- exercise02/instance.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "new instance" {
-    vpc_security_group_ids = [aws_security_group.mysg.id]
-    key_name = aws_key_pair.keypair.key_name
-    ami = data.aws_ami.ami_id01.id
-    availability_zone = "us-east-1a"
-    instance_type = "t3.micro"
-}
--- exercise02/key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "keypair"{
-    key_name = "newkey"
-    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA35XeLtbgxqinhveN6QqxcShL8tReKqDCH3AUc/FAjN nikhi@Nikhil"
-}
